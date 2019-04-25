eefb3c423ba9ab39196256dc0aa900ef3bd15f15
yangjiegeng <yangjiegeng@wacai.com>
2019-04-26 01:00:48 +0800

    feat: jdbc_springboot

---
+++ src/main/java/com/company/project/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.company.project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
+  @RequestMapping(value = "/user")
+  public Object person2() {
+    return jdbcTemplate.queryForList("SELECT * FROM t_user");
+  }
+
+}
---
+++ src/main/java/com/company/project/entity/User.java
@@ -0,0 +1,16 @@
+package com.company.project.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+  private String name;
+  private int age;
+}
